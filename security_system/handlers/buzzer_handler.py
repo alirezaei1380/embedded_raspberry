34ecd99bd7d2f4e1cff019d69e873f172aa694d2
Ali Rezaei <alirezaeim1380@gmail.com>
2023-02-07 16:22:37 +0330

    fix buzzer handler

--- security_system/handlers/buzzer_handler.py
+++ security_system/handlers/buzzer_handler.py
@@ -1,12 +1,21 @@
-from signal import pause
-
 from gpiozero import Buzzer
+from time import sleep
 
-BUZEER_PORT = 3
-
-bz = Buzzer(BUZEER_PORT)
+buzzer = Buzzer(17)
 
 
-def beep():
-    bz.beep(1, 1, 10)
-    pause()
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+
+
+def beep(repeat=3):
+   for i in range(0, repeat):
+      for pulse in range(60):
+         GPIO.output(17, True)
+         sleep(0.001)
+         GPIO.output(17, False)
+         sleep(0.001)
+      sleep(0.02)
