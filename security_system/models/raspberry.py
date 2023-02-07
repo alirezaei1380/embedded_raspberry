abafeace930517d09bd51682d5f6a7c6cb7da561
Ali Rezaei <alirezaeim1380@gmail.com>
2023-02-07 17:13:43 +0330

    fix models

--- security_system/models/raspberry.py
+++ security_system/models/raspberry.py
@@ -6,15 +6,7 @@ from django.db import models
 class Raspberry(models.Model):
 
     admin_code = models.IntegerField(
-        validators=[
-            MinValueValidator(100_000),
-            MaxValueValidator(900_000_000),
-        ],
     )
 
     user_code = models.IntegerField(
-        validators=[
-            MinValueValidator(100_000),
-            MaxValueValidator(900_000_000),
-        ],
     )
