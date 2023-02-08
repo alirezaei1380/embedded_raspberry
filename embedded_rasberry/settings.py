1b7e9cb8b446fc20e4d4d7bc936f49280a36ec8b
Ali Rezaei <alirezaeim1380@gmail.com>
2023-02-08 09:57:32 +0330

    fix key pad handler bug

--- embedded_rasberry/settings.py
+++ embedded_rasberry/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'embedded_rasberry.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR.join('db.sqlite3'),
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
